624d6fbb4b2128820528d44f47efbf18cc99a50b
Heiko Stein <hs@heiko-stein.de>
2020-07-30 20:30:56 +0200

    add variables.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
 //aws
 provider "aws" {
   profile = var.profile
@@ -8,19 +12,19 @@ provider "aws" {
 module "stack" {
   source = "./modules/stack"
 
-  vpc_name = "hs_test_vpc"
-  vpc_cidr = "10.0.0.0/16"
-  server_type = "hs_test"
+  vpc_name = join("_",[var.server_type, "vpc"])
+  vpc_cidr = var.vpc_cidr
+  server_type = var.server_type
 
-  subnet_name = "hs_test_subnet"
-  subnet_cidr = "10.0.1.0/24"
-  subnet_availability_zone = "eu-central-1a"
+  subnet_name = join("_",[var.server_type, "subnet"])
+  subnet_cidr = var.subnet_cidr
+  subnet_availability_zone = var.subnet_availability_zone
 
-  public_key_path= "~/.ssh/id_rsa.pub"
-  keypair_name= "hs_test_keypair"
+  public_key_path= var.public_key_path
+  keypair_name= join("_",[var.server_type, "keypair"])
 
-  instance_ami = "ami-0a02ee601d742e89f"
-  instance_type = "t2.nano"
+  instance_ami = var.instance_ami
+  instance_type = var.instance_type
   instance_count = var.instance_count
 }
 
