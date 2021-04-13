3fa2f772e7b942c64e11aa9b422fce73515da171
Heiko Stein <hs@heiko-stein.de>
2021-04-13 13:56:55 +0200

    add variable cidr to sec_group

--- modules/stack/priv_sec_groups.tf
+++ modules/stack/priv_sec_groups.tf
@@ -6,13 +6,13 @@ resource "aws_security_group" "priv" {
       from_port   = 22
       to_port     = 22
       protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = [var.pub_subnet_cidr]
   }
   ingress {
       from_port   = 8
       to_port     = 0
       protocol    = "icmp"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = [var.pub_subnet_cidr]
   }
  egress {
     from_port   = 0
