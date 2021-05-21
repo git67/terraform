2ffb6d27f5da6f34ae0984b51cd33fb3de6b4615
Heiko Stein <hs@heiko-stein.de>
2021-05-21 08:02:05 +0200

    add elb probe script

--- scripts/probe_elb.sh
+++ scripts/probe_elb.sh
@@ -1,14 +1,24 @@
-#!/bin/bash
+#!/bin/bash 
+set -euo pipefail
+
+export PATH="/usr/bin:/usr/sbin/:${PATH}"
 
 ELB=${1:-"NONE"}
+PORT=${2:-"8080"}
+PROBE_COUNT=${3:-"100"}
 
-if [ ${ELB} = "NONE" ]; then
+_probe_elb()
+{
+if [ ${1} = "NONE" ]; then
 	exit 1
 fi
 
 while true
 do
-	wget -q ${ELB}
+	wget -q ${ELB}:${PORT}
 	grep eth0 index.html
 	rm index.html*
 done
+}
+
+_probe_elb ${ELB} ${PORT}
